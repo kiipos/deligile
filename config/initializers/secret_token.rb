c491b8ca02ee369da1470594dd96474306ca28f4
kiipos <kiipos@delimount.com>
2011-06-27 13:50:53 +0900

    처음 커밋합니다!! 화이팅~

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deligile::Application.config.secret_token = '59fee5253420b77103b0b7a148612b27701011557b14cb7e65c8afc1028a5c6e3543afee7e9f0fba6556475014a984231989646db91c18067314e33f8a477c39'
