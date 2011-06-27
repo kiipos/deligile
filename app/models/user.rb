f15ae4d1245a4d69cb84bdd14f9edf8686fe64cc
kiipos <kiipos@delimount.com>
2011-06-27 15:37:22 +0900

    User   * encrypt_password 추가 - password와 salt를 이용해서 암호화   * password= 수정 - salt, encrypted_password 쓰기

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
 class User < ActiveRecord::Base
-  attr_reader( :password )
+  SEED = 'Beansgirl'
   
+  attr_reader( :password )
+  attr_accessible( :sign_in_id, :email, :password )
+
+  public
+  def self.encrypt_password( password, salt )
+    concatenated = password.to_s + User::SEED + salt
+    return Digest::SHA2.hexdigest( concatenated )
+  end
+ 
+  public
   def password=( value )
-    # ...
-    @password = value
+    salt = object_id.to_s() + rand.to_s()
+    write_attribute( :salt, salt )
+    
+    encrypted = User.encrypt_password( value, salt )
+    write_attribute( :encrypted_password, encrypted ) # self.encrypted_password = value <- 나쁜 소스
   end
 end
