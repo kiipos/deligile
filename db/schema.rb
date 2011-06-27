ecf497df039998cabafcf684d26913d9d1dfffc4
kiipos <kiipos@delimount.com>
2011-06-27 17:21:57 +0900

    AddTypeToUsers   * type 컬럼 추가
    Administrator
      * User로부터 상속

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110627045438) do
+ActiveRecord::Schema.define(:version => 20110627081951) do
 
   create_table "users", :force => true do |t|
     t.string   "sign_in_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110627045438) do
     t.string   "encrypted_password"
     t.string   "salt"
     t.datetime "created_at"
+    t.string   "type"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
