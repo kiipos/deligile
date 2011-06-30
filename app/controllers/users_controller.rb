98f950ebed687c070ce2863b97fd7daa7d0b6f34
Jo Ethan <joethan@Jo-Ethans-MacBook-Air.local>
2011-06-30 15:04:44 +0900

    AAdd comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   public
   def index()
     if nil == session[ :user_id ]
-      redirect_to( new_session_path ) and return
+      redirect_to( new_session_path ) and return #error no.1
     end
     
     @users = User.find( :all )
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   public
   def create()
     @user = User.create( params[ :user ] )
-    session[ :user_id ] = @user.id
+    session[ :user_id ] = @user.id #solution of error no.1
     redirect_to( users_path ) 
   end
   
